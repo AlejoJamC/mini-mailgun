d643e287942b7fc368b6f3e7d851c117f50ad249
Alejandro Mantilla <alejandromantilla7@hotmail.com>
2016-07-31 02:53:03 -0500

    mocha simple structure

---
+++ test/mini-mailgun.spec.js
@@ -0,0 +1,16 @@
+/**
+ * mini-mailgun: A lightweight Mailgun API client for Node.js.
+ *
+ * Copyright (c) 2016-present, Alejandro Mantilla <@AlejoJamC>.
+ * MIT License.
+ */
+
+var assert = require('assert');
+
+describe("A feature", function () {
+    describe("A scenario", function () {
+        it("does something expected", function () {
+            assert(true);
+        });
+    });
+});
